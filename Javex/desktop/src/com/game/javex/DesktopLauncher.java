c93c9aa77f08b0a8bb8a0b7216812d7ba6538fcc
Edmund Lin <elz.work22@gmail.com>
2024-02-03 01:29:21 +0800

    feat: create javex

---
+++ Javex/desktop/src/com/game/javex/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.game.javex;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.game.javex.Javex;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("P11T5");
+		new Lwjgl3Application(new Javex(), config);
+	}
+}
