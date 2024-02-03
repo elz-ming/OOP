cc7775f0496fd4ce991e195893a34c6b710a540a
jovanlim161 <jovanlim161@gmail.com>
2024-02-03 22:22:08 +0800

    feat: create scene manager
    libgdx lib for menu scene

--- Javex/core/src/com/game/javex/scenes/SceneManager.java
+++ Javex/core/src/com/game/javex/scenes/SceneManager.java
@@ -1,5 +1,36 @@
 package com.game.javex.scenes;
 
-public class SceneManager {
+import com.badlogic.gdx.Game;
 
+public class SceneManager extends Game {
+    public enum SceneType {
+        START_MENU,
+        PLAYING,
+        PAUSE,
+        END_LEADERBOARD
+    }
+
+    @Override
+    public void create() {
+        setScreen(new StartMenuScene(this));
+    }
+ // Start with the Start Menu Scene
+    
+
+    public void changeScene(SceneType sceneType) {
+        switch (sceneType) {
+            case START_MENU:
+                this.setScreen(new StartMenuScene(this));
+                break;
+            case PLAYING:
+                this.setScreen(new PlayingScene(this));
+                break;
+            case PAUSE:
+                this.setScreen(new PauseScene(this));
+                break;
+            case END_LEADERBOARD:
+                this.setScreen(new EndLeaderboardScene(this));
+                break;
+        }
+    }
 }
