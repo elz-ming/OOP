3c1aacf5dc7f3ab74147d3fbc5a9daa662beb868
Edmund Lin <elz.work22@gmail.com>
2024-02-07 15:01:22 +0800

    feat: create skeleton of PlayScene

---
+++ Javex/core/src/com/game/javex/scenes/PlayScene.java
@@ -0,0 +1,90 @@
+package com.game.javex.scenes;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class PlayScene extends AbstractScene {
+
+	protected PlayScene(SceneManager sceneManager) {
+		super(sceneManager);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void handleInput() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void handleInput(float dt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void update(float dt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void render(SpriteBatch sb) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
