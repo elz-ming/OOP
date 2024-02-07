7a77b14489d45aff097ebd9350f5b36c988690e2
jovanlim161 <jovanlim161@gmail.com>
2024-02-08 00:45:04 +0800

    feat: updating play and pause scene methods
    updating play and pause screen methods

--- Javex/core/src/com/game/javex/scenes/AbstractScene.java
+++ Javex/core/src/com/game/javex/scenes/AbstractScene.java
@@ -7,21 +7,25 @@ import com.game.javex.scenes.SceneManager;
 // This is the Scene Parent Class
 public abstract class AbstractScene implements Screen {
     protected SceneManager sceneManager;
+    protected SpriteBatch spriteBatch;
 
-    protected AbstractScene(SceneManager sceneManager) {
+    protected AbstractScene(SceneManager sceneManager, SpriteBatch spriteBatch) {
         this.sceneManager = sceneManager;
+        this.spriteBatch = spriteBatch;
     }
-    
-    // For Menu, Pause and End Scene
+
+    // For Menu, Pause, and End Scene
     protected abstract void handleInput();
     protected abstract void update();
-    protected abstract void render();
     
+    // You could provide a default implementation here if it makes sense for your game architecture
+    protected void render() {
+        // Default implementation could be empty
+    }
+
     // For Play Scene
     protected abstract void handleInput(float dt);
     protected abstract void update(float dt);
-    protected abstract void render(SpriteBatch sb);
-    
-    public abstract void dispose();
-    // Implement the common Screen methods here or leave them abstract to be implemented by subclasses
+    protected abstract void render(SpriteBatch spriteBatch);
+
 }
