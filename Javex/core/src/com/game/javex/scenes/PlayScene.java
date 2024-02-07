a51912893a09d3902938d7c9b9a2bf6516370932
jovanlim161 <jovanlim161@gmail.com>
2024-02-08 00:46:25 +0800

    feat: create playscene
    creating playscene

--- Javex/core/src/com/game/javex/scenes/PlayScene.java
+++ Javex/core/src/com/game/javex/scenes/PlayScene.java
@@ -1,11 +1,16 @@
 package com.game.javex.scenes;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class PlayScene extends AbstractScene {
 
+	private static SpriteBatch spriteBatch;
+
 	protected PlayScene(SceneManager sceneManager) {
-		super(sceneManager);
+		super(sceneManager, spriteBatch);
 		// TODO Auto-generated constructor stub
 	}
 
@@ -65,18 +70,24 @@ public class PlayScene extends AbstractScene {
 
 	@Override
 	protected void handleInput(float dt) {
-		// TODO Auto-generated method stub
-		
+	    if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+	        // Pause the game
+	        sceneManager.push(new PauseScene(sceneManager, spriteBatch));
+	    }
 	}
 
 	@Override
 	protected void update(float dt) {
+		handleInput(dt);
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	protected void render(SpriteBatch sb) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
 		// TODO Auto-generated method stub
 		
 	}
