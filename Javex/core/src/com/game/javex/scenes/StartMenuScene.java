3fa48eae5a41fc5fa9fcaa76d0bfccde7c751338
Edmund Lin <elz.work22@gmail.com>
2024-02-08 11:07:34 +0800

    fix: remove repetitive methods

--- Javex/core/src/com/game/javex/scenes/StartMenuScene.java
+++ Javex/core/src/com/game/javex/scenes/StartMenuScene.java
@@ -8,10 +8,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.game.javex.scenes.AbstractScene;
-import com.game.javex.scenes.SceneManager;
 
 public class StartMenuScene extends AbstractScene {
     private static SpriteBatch spriteBatch;
@@ -22,7 +19,8 @@ public class StartMenuScene extends AbstractScene {
     private int currentButtonIndex = 0;
 
     public StartMenuScene(SceneManager sceneManager) {
-        super(sceneManager, spriteBatch);
+        super(sceneManager);
+        
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
@@ -70,7 +68,7 @@ public class StartMenuScene extends AbstractScene {
         }
     }
 
-    public void handleInput() {
+    public void handleInput(float dt) {
         if (Gdx.input.isKeyJustPressed(Input.Keys.UP) || Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
             currentButtonIndex = (currentButtonIndex + 1) % menuButtons.length;
             updateButtonStyles();
@@ -86,12 +84,12 @@ public class StartMenuScene extends AbstractScene {
         }
     }
     
-    public void update() {
-    	handleInput();
+    public void update(float dt) {
+    	handleInput(dt);
     }  
 
 	@Override
-	protected void render() {
+	protected void render(SpriteBatch sb) {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
@@ -134,24 +132,6 @@ public class StartMenuScene extends AbstractScene {
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected void handleInput(float dt) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected void update(float dt) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected void render(SpriteBatch sb) {
 		// TODO Auto-generated method stub
 		
 	}	
