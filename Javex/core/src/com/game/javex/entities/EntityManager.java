0e5642eacc5b40f4f8baf45b64a8cc3d216274be
Edmund Lin <elz.work22@gmail.com>
2024-02-08 11:08:42 +0800

    feat: initialize the three managers in entity manager

--- Javex/core/src/com/game/javex/entities/EntityManager.java
+++ Javex/core/src/com/game/javex/entities/EntityManager.java
@@ -4,4 +4,15 @@ import com.game.javex.entities.enemies.EnemyManager;
 import com.game.javex.entities.players.PlayerManager;
 import com.game.javex.entities.terrains.TerrainManager;
 
+public class EntityManager {
+	
+	private PlayerManager playerManager;
+	private EnemyManager enemyManager;
+	private TerrainManager terrainManager;
+	
+	public EntityManager() {
+		playerManager = new PlayerManager();
+		enemyManager = new EnemyManager();
+		terrainManager = new TerrainManager();
+	}
 }
