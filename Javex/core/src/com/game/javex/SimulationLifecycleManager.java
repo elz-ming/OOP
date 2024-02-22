5c5b7e0da9bfd92859765ba7955ac458fb7cda2f
Edmund Lin <elz.work22@gmail.com>
2024-02-22 12:59:24 +0800

    chore: code house cleaning

--- Javex/core/src/com/game/javex/SimulationLifecycleManager.java
+++ Javex/core/src/com/game/javex/SimulationLifecycleManager.java
@@ -2,25 +2,24 @@ package com.game.javex;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-
 import com.game.javex.inouts.*;
 import com.game.javex.scenes.*;
 
 
 // This is our SIMULATION LIFECYCLE MANAGER
-public class SimulationLifecycleManager extends ApplicationAdapter {
+public class SimulationLifecycleManager extends ApplicationAdapter{
 	private SceneManager sceneManager;
 	private InputManager inputManager;
 	private OutputManager outputManager;
 	
-	 @Override
-	 public void create() {
-		 inputManager = new InputManager();
-	     outputManager = new OutputManager(); // Initialize AudioManager here
-	     
-	     sceneManager = new SceneManager(); // Assume SceneManager modified to accept AudioManager
-	     sceneManager.push(new MenuScene(sceneManager, inputManager, outputManager)); // Pass AudioManager to your scenes if needed
-	 }
+	@Override
+	public void create() {
+		inputManager = new InputManager();
+		outputManager = new OutputManager();
+		sceneManager = new SceneManager();
+		 
+		sceneManager.push(new MenuScene(sceneManager, inputManager, outputManager));
+	}
 
 	@Override
 	public void render() {
